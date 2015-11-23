184641658895f7d0435a8a2ec2e813f4c8b8f52c
Tyler Reitz <tdreitz@gmail.com>
2015-11-23 15:05:39 -0800

    fb-angular-routing-states

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,26 @@
 var blocJams = angular.module('blocJams', ['ui.router']);
+
+blocJams.config(function($stateProvider, $locationProvider) {
+  
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+
+  $stateProvider
+    .state('landing', {
+      url: '/',
+      // controller: 'landing.controller',
+      templateUrl:  '/templates/landing.html'
+    })
+    .state('collection', {
+      url: '/collection',
+      // controller: 'collection.controller',
+      templateUrl: '/templates/collection.html'
+    })
+    .state('album', {
+      url: '/album',
+      // controller: 'album.controller',
+      templateUrl: '/templates/album.html'
+    })
+});
